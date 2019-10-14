d4f263ae5fcffeb496c17eeec7bd1a6e3952081a
chinch2 <johao5991@gmail.com>
2019-10-14 15:44:10 -0400

    Empezando a montar el codigo para backend

--- backend/tabla2.php
+++ backend/tabla2.php
@@ -2,97 +2,96 @@
 <?php
 
 session_start();
-if(@!$_SESSION['Usuario']){
-        header("location:login.php");
+if (@!$_SESSION['Usuario']) {
+  header("location:login.php");
 }
-if($_SESSION['Usuario']!='admin'){
-	header("Location:logout.php");
+if ($_SESSION['Usuario'] != 'admin') {
+  header("Location:logout.php");
 }
 
 ?>
 
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Desarrollos PNP | Dash Text</title>
-    </head>
-    <body>
-Tabla de usuarios
-        <?php
-        include("conn.php");
 
-        if(isset($_GET["del"])){
+<head>
+  <title>Desarrollos PNP | Dash Text</title>
+</head>
 
-          $sql = "DELETE FROM sistema.usuarios WHERE UserID = '".$_GET["id"]."';";
+<body>
+  Tabla de usuarios
+  <?php
+  include("conn.php");
 
-          if ($conn->query($sql) === TRUE) {
+  if (isset($_GET["del"])) {
 
-          $last_id = $conn->insert_id;
+    $sql = "DELETE FROM sistema.usuarios WHERE UserID = '" . $_GET["id"] . "';";
 
-          $_GET["id"] = $last_id;
+    if ($conn->query($sql) === TRUE) {
 
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        unset($_GET["del"]);
+      $last_id = $conn->insert_id;
 
-        }
+      $_GET["id"] = $last_id;
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    unset($_GET["del"]);
+  }
 
-        if(isset($_GET["insert"])){
+  if (isset($_GET["insert"])) {
 
-        $sql = "INSERT INTO usuarios (Usuario, Clave, Niveldeacceso) VALUES ('supervisor', '240295', 0);";
+    $sql = "INSERT INTO usuarios (Usuario, Clave, Niveldeacceso) VALUES ('supervisor', '240295', 0);";
 
-          if ($conn->query($sql) === TRUE) {
+    if ($conn->query($sql) === TRUE) {
 
-          $last_id = $conn->insert_id;
+      $last_id = $conn->insert_id;
 
-          $_GET["id"] = $last_id;
+      $_GET["id"] = $last_id;
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+  }
 
-          } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-      }
- 
-        if(isset($_POST["forma"])){
-            $sql = "UPDATE sistema.usuarios SET Usuario = '{$_POST["Usuario"]}', Clave = '{$_POST["Clave"]}', Niveldeacceso = '{$_POST["Niveldeacceso"]}' WHERE UserID = '{$_POST["UserID"]}';";
-            if($conn->query($sql)===TRUE){
-            	unset($_GET["id"]);
-	    }else{
-			echo "Error: " . $sql . "<br>" . $conn->error;
-	 	 } 
-        }
-        ?>
+  if (isset($_POST["forma"])) {
+    $sql = "UPDATE sistema.usuarios SET Usuario = '{$_POST["Usuario"]}', Clave = '{$_POST["Clave"]}', Niveldeacceso = '{$_POST["Niveldeacceso"]}' WHERE UserID = '{$_POST["UserID"]}';";
+    if ($conn->query($sql) === TRUE) {
+      unset($_GET["id"]);
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+  }
+  ?>
   <a href="index.php">Inicio</a>
   <table border="1">
-     <tr>
-          <th>Usuario</th>
-          <th>Clave</th>
-          <th>Nivel de acceso</th>
-      </tr>
-<?php
-$sql = "SELECT * FROM sistema.usuarios";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-        while($row = $result-> fetch_assoc()){
-                echo "<tr><td>{$row["Usuario"]}</td><td>{$row["Clave"]}</td><td>{$row["Niveldeacceso"]}</td>";
-                if($_SESSION['Access']==1){
-                        echo "<td><a href=\"tabla5.php?id={$row["UserID"]}\">Modificar</a></td><td><a href=\"tabla5.php?id={$row["UserID"]}&del=1\">Eliminar</a></td></tr>";
-                }else{
-                echo "</tr>";
-                }
-
-                if($_GET["id"]==$row["UserID"]){
-                        $campo = $row;
-                }
+    <tr>
+      <th>Usuario</th>
+      <th>Clave</th>
+      <th>Nivel de acceso</th>
+    </tr>
+    <?php
+    $sql = "SELECT * FROM sistema.usuarios";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<tr><td>{$row["Usuario"]}</td><td>{$row["Clave"]}</td><td>{$row["Niveldeacceso"]}</td>";
+        if ($_SESSION['Access'] == 1) {
+          echo "<td><a href=\"tabla5.php?id={$row["UserID"]}\">Modificar</a></td><td><a href=\"tabla5.php?id={$row["UserID"]}&del=1\">Eliminar</a></td></tr>";
+        } else {
+          echo "</tr>";
         }
-}else{
-  echo "0 results <br><br>";
-}
 
-echo "</table><br><br>";
+        if ($_GET["id"] == $row["UserID"]) {
+          $campo = $row;
+        }
+      }
+    } else {
+      echo "0 results <br><br>";
+    }
 
-if(count($campo)){
-    echo '<form action= "tabla5.php" method="post">
+    echo "</table><br><br>";
+
+    if (count($campo)) {
+      echo '<form action= "tabla5.php" method="post">
  Usuario:<br>
   <input type="text" name="Usuario" value="' . $campo["Usuario"] . '">
   <br>
@@ -105,17 +104,17 @@ if(count($campo)){
   <br>
   <input type="submit" name="forma" value="Submit">
  </form>';
-}
-if(!isset($_GET["insert"]) && $_SESSION['Access']==1){
+    }
+    if (!isset($_GET["insert"]) && $_SESSION['Access'] == 1) {
 
-  echo '<a href="tabla5.php?insert=1">Ingresar nuevo registro</a>';
-}
+      echo '<a href="tabla5.php?insert=1">Ingresar nuevo registro</a>';
+    }
 
 
-$conn->close();
-?> 
+    $conn->close();
+    ?>
 
   </table>
-    </body>
-</html>
+</body>
 
+</html>
