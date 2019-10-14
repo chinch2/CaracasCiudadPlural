9d7199f81695725caf49d2dff00e9441c92bc82f
chinch2 <johao5991@gmail.com>
2019-10-14 15:40:14 -0400

    Creando db tablas y arreglando coneccion

--- backend/conn.php
+++ backend/conn.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "127.0.0.1";
-$username = "raspberry";
+$username = "root";
 $password = "J.2571582.j";
 $dbname = "sistema";
 
@@ -11,5 +11,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-?>
