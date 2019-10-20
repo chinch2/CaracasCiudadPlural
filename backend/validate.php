8b645f90c841c2c2c988f9ed275507f0429ae952
Jose Arguello <johao5991@gmail.com>
2019-10-20 01:34:55 -0400

    Eliminando el user== admin. correccion de seguridad

--- backend/validate.php
+++ backend/validate.php
@@ -7,7 +7,6 @@ require("conn.php");
 $username = $_POST['user'];
 $pass = $_POST['pass'];
 
-if ($username == 'admin') {
 	$sql = "SELECT * FROM users WHERE user='$username'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
@@ -16,7 +15,7 @@ if ($username == 'admin') {
 				$_SESSION['ID'] = $row['id'];
 				$_SESSION['Usuario'] = $row['user'];
 				$_SESSION['Access'] = $row['access_level'];
-				echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
+				echo '<script>alert("BIENVENIDO")</script> ';
 				echo "<script>location.href='tablas.php'</script>";
 			} else {
 				echo '<script>alert("Los Datos ingresados no son los correctos")</script> ';
@@ -24,21 +23,3 @@ if ($username == 'admin') {
 			}
 		}
 	}
-}
-
-$sql = "SELECT * FROM users WHERE user='$username'";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-	while ($row = $result->fetch_assoc()) {
-		if ($pass == $row['password']) {
-			$_SESSION['ID'] = $row['id'];
-			$_SESSION['Usuario'] = $row['user'];
-			$_SESSION['Access'] = $row['access_level'];
-			echo '<script>alert("BIENVENIDO SUPERVISOR")</script> ';
-			echo "<script>location.href='tablas.php'</script>";
-		}
-	}
-} else {
-	echo '<script>alert("Los Datos ingresados no son los correctos")</script> ';
-	echo "<script>location.href='login.php'</script>";
-}
